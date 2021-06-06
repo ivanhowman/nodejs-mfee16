0b993678753cfc92e50ee172a856f683a190f03c
ivanhowman <ivanhowman@gmail.com>
2021-06-06 10:49:29 +0800

    env update

--- crawler/app.js
+++ crawler/app.js
@@ -5,13 +5,15 @@ const fs = require("fs/promises");
 const moment = require("moment");
 const mysql = require("mysql");
 const Promise = require("bluebird");
+require("dotenv").config();
 
 //建立連線所需資料
 let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "stock",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 //建立連線
 connection = Promise.promisifyAll(connection);
